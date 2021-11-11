f6ee8d9dfbe083f8120b425993528686e9e9625f
nashon <kimutainashon@gmail.com>
2021-11-11 15:06:34 +0300

    changes

--- lib/widgets/authentication/email_password/ep_register_form.dart
+++ lib/widgets/authentication/email_password/ep_register_form.dart
@@ -54,144 +54,147 @@ class _EPRegisterFormState extends State<EPRegisterForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _registerFormKey,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(
-              left: 8.0,
-              right: 8.0,
-            ),
-            child: Column(
-              children: [
-                CustomFormField(
-                  controller: _nameController,
-                  focusNode: widget.nameFocusNode,
-                  keyboardType: TextInputType.name,
-                  inputAction: TextInputAction.next,
-                  isCapitalized: true,
-                  validator: (value) => EPValidator.validateName(
-                    name: value,
-                  ),
-                  label: 'Name',
-                  hint: 'Enter your name',
-                ),
-                SizedBox(height: 16.0),
-                CustomFormField(
-                  controller: _emailController,
-                  focusNode: widget.emailFocusNode,
-                  keyboardType: TextInputType.emailAddress,
-                  inputAction: TextInputAction.next,
-                  validator: (value) => EPValidator.validateEmail(
-                    email: value,
-                  ),
-                  label: 'Email',
-                  hint: 'Enter your email',
-                ),
-                SizedBox(height: 16.0),
-                CustomFormField(
-                  controller: _passwordController,
-                  focusNode: widget.passwordFocusNode,
-                  keyboardType: TextInputType.text,
-                  inputAction: TextInputAction.done,
-                  validator: (value) => EPValidator.validatePassword(
-                    password: value,
-                  ),
-                  isObscure: true,
-                  label: 'Password',
-                  hint: 'Enter your password',
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: 24.0),
-          _isSingningUp
-              ? Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: CircularProgressIndicator(
-                    valueColor: AlwaysStoppedAnimation<Color>(
-                      Palette.firebaseOrange,
+    return Scaffold(
+      backgroundColor:Palette.firebaseNavy,
+      body: Form(
+        key: _registerFormKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 8.0,
+                right: 8.0,
+              ),
+              child: Column(
+                children: [
+                  CustomFormField(
+                    controller: _nameController,
+                    focusNode: widget.nameFocusNode,
+                    keyboardType: TextInputType.name,
+                    inputAction: TextInputAction.next,
+                    isCapitalized: true,
+                    validator: (value) => EPValidator.validateName(
+                      name: value,
                     ),
+                    label: 'Name',
+                    hint: 'Enter your name',
                   ),
-                )
-              : Padding(
-                  padding: EdgeInsets.only(left: 0.0, right: 0.0),
-                  child: Container(
-                    width: double.maxFinite,
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(
-                          Palette.firebaseOrange,
-                        ),
-                        shape: MaterialStateProperty.all(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                      ),
-                      onPressed: () async {
-                        widget.emailFocusNode.unfocus();
-                        widget.passwordFocusNode.unfocus();
-
-                        setState(() {
-                          _isSingningUp = true;
-                        });
-
-                        if (_registerFormKey.currentState!.validate()) {
-                          User? user =
-                              await EPAuthentication.registerUsingEmailPassword(
-                            name: _nameController.text,
-                            email: _emailController.text,
-                            password: _passwordController.text,
-                            context: context,
-                          );
-
-                          if (user != null) {
-                            Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(
-                        builder: (context) => DbDashboardScreen(),
-                      ),
-                    );
-                          }
-                        }
-
-                        setState(() {
-                          _isSingningUp = false;
-                        });
-                      },
-                      child: Padding(
-                        padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
-                        child: Text(
-                          'REGISTER',
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            color: Palette.firebaseGrey,
-                            letterSpacing: 2,
-                          ),
-                        ),
-                      ),
+                  SizedBox(height: 16.0),
+                  CustomFormField(
+                    controller: _emailController,
+                    focusNode: widget.emailFocusNode,
+                    keyboardType: TextInputType.emailAddress,
+                    inputAction: TextInputAction.next,
+                    validator: (value) => EPValidator.validateEmail(
+                      email: value,
                     ),
+                    label: 'Email',
+                    hint: 'Enter your email',
                   ),
-                ),
-          SizedBox(height: 16.0),
-          InkWell(
-            onTap: () {
-              Navigator.of(context).pushReplacement(
-                _routeToSignInScreen(),
-              );
-            },
-            child: Text(
-              'Already have an account? Sign in',
-              style: TextStyle(
-                color: Palette.firebaseGrey,
-                letterSpacing: 0.5,
+                  SizedBox(height: 16.0),
+                  CustomFormField(
+                    controller: _passwordController,
+                    focusNode: widget.passwordFocusNode,
+                    keyboardType: TextInputType.text,
+                    inputAction: TextInputAction.done,
+                    validator: (value) => EPValidator.validatePassword(
+                      password: value,
+                    ),
+                    isObscure: true,
+                    label: 'Password',
+                    hint: 'Enter your password',
+                  ),
+                ],
               ),
             ),
-          )
-        ],
+            SizedBox(height: 24.0),
+            _isSingningUp
+                ? Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(
+                        Palette.firebaseOrange,
+                      ),
+                    ),
+                  )
+                : Padding(
+                    padding: EdgeInsets.only(left: 0.0, right: 0.0),
+                    child: Container(
+                      width: double.maxFinite,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                            Palette.firebaseOrange,
+                          ),
+                          shape: MaterialStateProperty.all(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        ),
+                        onPressed: () async {
+                          widget.emailFocusNode.unfocus();
+                          widget.passwordFocusNode.unfocus();
+    
+                          setState(() {
+                            _isSingningUp = true;
+                          });
+    
+                          if (_registerFormKey.currentState!.validate()) {
+                            User? user =
+                                await EPAuthentication.registerUsingEmailPassword(
+                              name: _nameController.text,
+                              email: _emailController.text,
+                              password: _passwordController.text,
+                              context: context,
+                            );
+    
+                            if (user != null) {
+                              Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                          builder: (context) => DbDashboardScreen(),
+                        ),
+                      );
+                            }
+                          }
+    
+                          setState(() {
+                            _isSingningUp = false;
+                          });
+                        },
+                        child: Padding(
+                          padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
+                          child: Text(
+                            'REGISTER',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Palette.firebaseGrey,
+                              letterSpacing: 2,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+            SizedBox(height: 16.0),
+            InkWell(
+              onTap: () {
+                Navigator.of(context).pushReplacement(
+                  _routeToSignInScreen(),
+                );
+              },
+              child: Text(
+                'Already have an account? Sign in',
+                style: TextStyle(
+                  color: Palette.firebaseGrey,
+                  letterSpacing: 0.5,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/authentication/email_password/ep_sign_in_form.dart
+++ lib/widgets/authentication/email_password/ep_sign_in_form.dart
@@ -31,131 +31,150 @@ class _EPSignInFormState extends State<EPSignInForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _signInFormKey,
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(
-              left: 8.0,
-              right: 8.0,
-              bottom: 24.0,
-            ),
-            child: Column(
-              children: [
-                CustomFormField(
-                  controller: _emailController,
-                  focusNode: emailFocusNode,
-                  keyboardType: TextInputType.emailAddress,
-                  inputAction: TextInputAction.next,
-                  validator: (value) => EPValidator.validateEmail(
-                    email: value,
-                  ),
-                  label: 'Email',
-                  hint: 'Enter your email',
-                ),
-                SizedBox(height: 16.0),
-                CustomFormField(
-                  controller: _passwordController,
-                  focusNode: passwordFocusNode,
-                  keyboardType: TextInputType.text,
-                  inputAction: TextInputAction.done,
-                  validator: (value) => EPValidator.validatePassword(
-                    password: value,
-                  ),
-                  isObscure: true,
-                  label: 'Password',
-                  hint: 'Enter your password',
-                ),
-              ],
-            ),
-          ),
-          _isSigningIn
-              ? Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: CircularProgressIndicator(
-                    valueColor: AlwaysStoppedAnimation<Color>(
-                      Palette.firebaseOrange,
-                    ),
-                  ),
-                )
-              : Padding(
-                  padding: EdgeInsets.only(left: 0.0, right: 0.0),
-                  child: Container(
-                    width: double.maxFinite,
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(
-                          Palette.firebaseOrange,
-                        ),
-                        shape: MaterialStateProperty.all(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
+    return Scaffold(
+      backgroundColor:  Palette.firebaseNavy,
+      appBar:AppBar(
+        backgroundColor:  Palette.firebaseNavy,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+
+        child: Column(
+          
+          children: [
+            SizedBox(height:30),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Form(
+                key: _signInFormKey,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 8.0,
+                        right: 8.0,
+                        bottom: 24.0,
+                      ),
+                      child: Column(
+                        children: [
+                          CustomFormField(
+                            controller: _emailController,
+                            focusNode: emailFocusNode,
+                            keyboardType: TextInputType.emailAddress,
+                            inputAction: TextInputAction.next,
+                            validator: (value) => EPValidator.validateEmail(
+                              email: value,
+                            ),
+                            label: 'Email',
+                            hint: 'Enter your email',
                           ),
-                        ),
+                          SizedBox(height: 16.0),
+                          CustomFormField(
+                            controller: _passwordController,
+                            focusNode: passwordFocusNode,
+                            keyboardType: TextInputType.text,
+                            inputAction: TextInputAction.done,
+                            validator: (value) => EPValidator.validatePassword(
+                              password: value,
+                            ),
+                            isObscure: true,
+                            label: 'Password',
+                            hint: 'Enter your password',
+                          ),
+                        ],
                       ),
-                      onPressed: () async {
-                        emailFocusNode.unfocus();
-                        passwordFocusNode.unfocus();
-
-                        setState(() {
-                          _isSigningIn = true;
-                        });
-
-                        if (_signInFormKey.currentState!.validate()) {
-                          User? user =
-                              await EPAuthentication.signInUsingEmailPassword(
-                            context: context,
-                            email: _emailController.text,
-                            password: _passwordController.text,
-                          );
-
-                          if (user != null) {
-                           Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(
-                        builder: (context) => DbDashboardScreen(),
-                      ),
-                    );
-                          }
-                        }
-
-                        setState(() {
-                          _isSigningIn = false;
-                        });
+                    ),
+                    _isSigningIn
+                        ? Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: CircularProgressIndicator(
+                              valueColor: AlwaysStoppedAnimation<Color>(
+                                Palette.firebaseOrange,
+                              ),
+                            ),
+                          )
+                        : Padding(
+                            padding: EdgeInsets.only(left: 0.0, right: 0.0),
+                            child: Container(
+                              width: double.maxFinite,
+                              child: ElevatedButton(
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                    Palette.firebaseOrange,
+                                  ),
+                                  shape: MaterialStateProperty.all(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                  ),
+                                ),
+                                onPressed: () async {
+                                  emailFocusNode.unfocus();
+                                  passwordFocusNode.unfocus();
+                
+                                  setState(() {
+                                    _isSigningIn = true;
+                                  });
+                
+                                  if (_signInFormKey.currentState!.validate()) {
+                                    User? user =
+                                        await EPAuthentication.signInUsingEmailPassword(
+                                      context: context,
+                                      email: _emailController.text,
+                                      password: _passwordController.text,
+                                    );
+                
+                                    if (user != null) {
+                                     Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(
+                                  builder: (context) => DbDashboardScreen(),
+                                ),
+                              );
+                                    }
+                                  }
+                
+                                  setState(() {
+                                    _isSigningIn = false;
+                                  });
+                                },
+                                child: Padding(
+                                  padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
+                                  child: Text(
+                                    'LOGIN',
+                                    style: TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold,
+                                      color: Palette.firebaseGrey,
+                                      letterSpacing: 2,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                    SizedBox(height: 16.0),
+                    InkWell(
+                      onTap: () {
+                        Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(
+                            builder: (context) => EPRegisterScreen(),
+                          ),
+                        );
                       },
-                      child: Padding(
-                        padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
-                        child: Text(
-                          'LOGIN',
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            color: Palette.firebaseGrey,
-                            letterSpacing: 2,
-                          ),
+                      child: Text(
+                        'Don\'t have an account? Sign up',
+                        style: TextStyle(
+                          color: Palette.firebaseGrey,
+                          letterSpacing: 0.5,
                         ),
                       ),
-                    ),
-                  ),
+                    )
+                  ],
                 ),
-          SizedBox(height: 16.0),
-          InkWell(
-            onTap: () {
-              Navigator.of(context).pushReplacement(
-                MaterialPageRoute(
-                  builder: (context) => EPRegisterScreen(),
-                ),
-              );
-            },
-            child: Text(
-              'Don\'t have an account? Sign up',
-              style: TextStyle(
-                color: Palette.firebaseGrey,
-                letterSpacing: 0.5,
               ),
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
